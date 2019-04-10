1fb14e678e7f57de10c5bb24d4975ca17ad4aa48
Daniel Arita <danielarita92@gmail.com>
2019-04-10 07:07:43 -0600

    Add authentication protection via JWT

--- product.routes.js
+++ product.routes.js
@@ -1,18 +1,20 @@
+const middleware = require('./middleware')
+
 module.exports = (app) => {
     const products = require('./product.controller.js');
 
     // Create a new Product
-    app.post('/products', products.create);
+    app.post('/products', middleware.isAuthorized, middleware.hasRoles(['admin']), products.create);
 
     // Retrieve all Products
-    app.get('/products', products.findAll);
+    app.get('/products', middleware.isAuthorized, products.findAll);
 
     // Retrieve a single Product with productId
-    app.get('/products/:productId', products.findOne);
+    app.get('/products/:productId', middleware.isAuthorized, products.findOne);
 
     // Update a Note with productId
-    app.put('/products/:productId', products.update);
+    app.put('/products/:productId', middleware.isAuthorized, middleware.hasRoles(['admin']), products.update);
 
     // Delete a Note with productId
-    app.delete('/products/:productId', products.delete);
+    app.delete('/products/:productId', middleware.isAuthorized, middleware.hasRoles(['admin']), products.delete);
 }
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Application-Key, Authorization");
     next();
   });
 
@@ -20,6 +20,7 @@ app.use(function(req, res, next) {
 const config = require('./config.js');
 const mongoose = require('mongoose');
 require('./product.routes.js')(app);
+require('./authorize.routes.js')(app);
 
 mongoose.Promise = global.Promise;
 
