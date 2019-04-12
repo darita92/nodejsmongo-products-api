749b6cb82c4771046ededd01730d5db4b7aa91e7
Daniel Arita <darita@HN-LNOHN0211m.loe.corp>
2019-04-12 15:16:58 -0600

    making mongo url configurable

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/products',
+    url: process.env.MONGO_URL || 'mongodb://localhost:27017/products',
     serverport: 3000,
     secret: '0194e36a-a6f4-4ce3-aabb-3ad270f10e9c',
     appKey: 'fe1d1cf9-b035-4025-98a3-d3f192d9e8e0'
